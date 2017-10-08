1f68ea68b6b8f6433cb8489f6dd9d4253e5b4093
fuzzylita <lita.stephenson@gmail.com>
2017-10-08 16:45:03 -0700

    Database and bcrypt setup

--- db/migrate/20171008231741_create_drinks.rb
+++ db/migrate/20171008231741_create_drinks.rb
@@ -1,7 +1,10 @@
 class CreateDrinks < ActiveRecord::Migration
   def change
     create_table :drinks do |t|
-
+      t.string :name
+      t.string :ingredients
+      t.string :instructions
+      t.integer :user_id
       t.timestamps null: false
     end
   end
--- db/migrate/20171008231807_create_ingredients.rb
+++ db/migrate/20171008231807_create_ingredients.rb
@@ -1,7 +1,8 @@
 class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
-
+      t.string :name
+      t.string :amount
       t.timestamps null: false
     end
   end
